bff56ed27c1ac07df209c6b662056241583f02e5
Khode4li <88293279+Khode4li@users.noreply.github.com>
2024-11-25 12:16:43 +0330

    fix length (4k character limit)

--- modules/telegram.php
+++ modules/telegram.php
@@ -27,19 +27,51 @@ class telegram extends base
 GET: " . $q['GET'] . "
 " . ((!is_null($q['POST'])) ? "POST: ". mdEscape($q['POST']) : "") . "
 ```" . "\n 〽️*headers* \n```\n" . mdEscape(getHeadersText()) . "```";
-        $sendMessageArray = [
-            'chat_id' => $this->CHAT_ID,
-            'text' => $message,
-            'parse_mode' => 'markdownv2'
-        ];
-        if(registry::get('THREAD_ID') !== ''){
-            $sendMessageArray['message_thread_id'] = registry::get('THREAD_ID');
+        if (strlen($message) > 4000) {
+            // Message is too long; send as a file
+            $tempFile = tempnam(sys_get_temp_dir(), 'tg_message_');
+            file_put_contents($tempFile, $message);
+
+            $this->sendFile($tempFile, 'message.txt');
+            unlink($tempFile); // Clean up temp file
+        }else{
+            $sendMessageArray = [
+                'chat_id' => $this->CHAT_ID,
+                'text' => $message,
+                'parse_mode' => 'markdownv2'
+            ];
+            if(registry::get('THREAD_ID') !== ''){
+                $sendMessageArray['message_thread_id'] = registry::get('THREAD_ID');
+            }
+            $this->sendMessage($sendMessageArray);
         }
-        $this->sendMessage($sendMessageArray);
     }
     private function sendMessage($params)
     {
         $url = "https://api.telegram.org/bot" . $this->TOKEN . "/sendMessage";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_exec($ch);
+        curl_close($ch);
+    }
+
+    private function sendFile($filePath, $fileName): void
+    {
+        $url = "https://api.telegram.org/bot" . $this->TOKEN . "/sendDocument";
+        $cfile = curl_file_create($filePath, 'text/plain', $fileName);
+
+        $params = [
+            'chat_id' => $this->CHAT_ID,
+            'document' => $cfile
+        ];
+
+        if (registry::get('THREAD_ID') !== '') {
+            $params['message_thread_id'] = registry::get('THREAD_ID');
+        }
+
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_POST, 1);
